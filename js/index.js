bb8e3f2b7eb6f57d29d8c5e149924f200c3b6e0e
Dave Steele <steelemediasolutions@gmail.com>
2021-04-01 14:24:12 -0400

    Added custom cursor and audio

--- js/index.js
+++ js/index.js
@@ -5,3 +5,15 @@ function randomHits() {
     document.getElementById('hits').innerHTML = hitCount;
 };
 randomHits();
+
+const media_element = document.getElementById("audio_element");
+
+function play_media() {
+    if (media_element.paused) {
+        media_element.play();
+    } else {
+        media_element.pause();
+    }
+}
+
+document.body.addEventListener('click', play_media);
