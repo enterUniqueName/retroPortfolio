c00db50d13ba968211bec84180f0fd2e771aa930
Dave Steele <steelemediasolutions@gmail.com>
2021-05-19 14:16:54 -0400

    Created real visitor counter, and added mobile navigation

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 
-function randomHits() {
-    var hitCount = Math.random() * (99999999 - 1) + 1;
-    hitCount = Math.round(hitCount);
-    document.getElementById('hits').innerHTML = hitCount;
-};
-randomHits();
+// function randomHits() {
+//     var hitCount = Math.random() * (99999999 - 1) + 1;
+//     hitCount = Math.round(hitCount);
+//     document.getElementById('hits').innerHTML = hitCount;
+// };
+// randomHits();
 
 const media_element = document.getElementById("audio_element");
 function play_media() {
@@ -16,3 +16,14 @@ function play_media() {
 }
 
 document.body.addEventListener('click', play_media);
+
+// Increment the custom counter by using increment.build
+function reqListener () {
+    console.log(this.responseText);
+    document.getElementById('hits').innerHTML = this.responseText;
+}
+  
+var oReq = new XMLHttpRequest();
+oReq.addEventListener("load", reqListener);
+oReq.open("GET", "https://increment.build/ananda-fetherston");
+oReq.send();
